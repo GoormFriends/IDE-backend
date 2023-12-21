af672329d881f6ef65b8f3c6667cdd142c906461
user <315luz10@gmail.com>
2023-12-21 17:44:05 +0900

    feat: 트랜잭선 관리, null 예외처리

--- backend/src/main/java/com/goorm/goormfriends/api/compiler/service/IdeService.java
+++ backend/src/main/java/com/goorm/goormfriends/api/compiler/service/IdeService.java
@@ -11,6 +11,7 @@ import com.goorm.goormfriends.db.repository.ProblemRepository;
 import com.goorm.goormfriends.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +21,7 @@ public class IdeService {
     private final UserRepository userRepository;
     private final IdeCompilerService ideCompilerService;
 
+    @Transactional
     public IdeResponse inputSaveAndCompile(Long userId, Long problemId, IdeRequest request) {
         // 현재 활성 문제 찾기
         Problem problem = problemRepository.findById(problemId)
@@ -32,8 +34,16 @@ public class IdeService {
         Ide ide = new Ide();
         ide.setUser(user);
         ide.setProblem(problem);
-        ide.setUsercode(request.getUsercode());
+        // usercode에 대한 널 체크와 기본값 설정
+        String defaultUserCode = "기본 코드"; // 필요에 따라 변경
+        ide.setUsercode(request.getUsercode() != null && !request.getUsercode().isEmpty() ? request.getUsercode() : defaultUserCode);
+
         ideRepository.save(ide);
+
+        // ide 상태가 null이 아닌지 확인
+        if (ide.getState() != null) {
+            // 상태가 설정되어 있으면 처리
+        }
 
         // 컴파일러 서비스를 통해 코드 실행 및 결과 검증
         ideCompilerService.executeCode(ide.getId());
